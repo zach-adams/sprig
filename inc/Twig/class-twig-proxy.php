71b86419b5b6e2e72f0d8bb17a0dcdc8202f8bfd
Zach <zach.adams383@gmail.com>
2014-12-06 14:08:49 -0700

    Added Twig Proxy class

---
+++ inc/Twig/class-twig-proxy.php
@@ -0,0 +1,15 @@
+<?php
+
+/*
+ * The Proxy that allows us to use Wordpress functions in Twig Templates
+ */
+class OurTwigProxy {
+    public function __call($function, $arguments) {
+
+        if (!function_exists($function)) {
+            trigger_error('call to unexisting function ' . $function, E_USER_ERROR);
+            return NULL;
+        }
+        return call_user_func_array($function, $arguments);
+    }
+}

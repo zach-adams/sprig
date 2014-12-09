3496bd9ca51cbd446b1836a4faf034948d2d63b9
Zach <zach.adams383@gmail.com>
2014-12-09 14:44:42 -0700

    Rearranged includes a little

--- functions.php
+++ functions.php
@@ -8,17 +8,17 @@
  * Please note that missing files will produce a fatal error.
  */
 $sprig_includes = array(
-  'inc/utils.php',                  // Utility functions
-  'inc/init.php',                   // Initial theme setup and constants
-  'inc/config.php',                 // Configuration
-  'inc/activation.php',             // Theme activation
-  'inc/titles.php',                 // Page titles
-  'inc/wp_bootstrap_navwalker.php', // Bootstrap Nav Walker (From https://github.com/twittem/wp-bootstrap-navwalker)
-  'inc/gallery.php',                // Custom [gallery] modifications
-  'inc/comments.php',               // Custom comments modifications
-  'inc/scripts.php',                // Scripts and stylesheets
-  'inc/extras.php',                  // Custom functions
-  'inc/twigpress.php'                   // Load TwigPress Engine
+	'inc/utils.php',                  // Utility functions
+	'inc/init.php',                   // Initial theme setup and constants
+	'inc/config.php',                 // Configuration
+	'inc/activation.php',             // Theme activation
+	'inc/titles.php',                 // Page titles
+	'inc/wp_bootstrap_navwalker.php', // Bootstrap Nav Walker (From https://github.com/twittem/wp-bootstrap-navwalker)
+	'inc/gallery.php',                // Custom [gallery] modifications
+	'inc/comments.php',               // Custom comments modifications
+	'inc/scripts.php',                // Scripts and stylesheets
+	'inc/twigpress.php',                   // Load TwigPress Engine
+	'inc/extras.php',                  // Custom functions
 );
 
 foreach ($sprig_includes as $file) {
